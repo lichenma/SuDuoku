54258dc0854d4153cf90c1ad6f44e0d2b5155d4b
Lichen Ma <lichenma123@gmail.com>
2020-12-12 03:18:11 -0500

    adding config files for deployment

--- src/components/Socket/Socket.js
+++ src/components/Socket/Socket.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react'
 import queryString from 'query-string'
 import io from 'socket.io-client'
 
-const ENDPOINT = 'localhost:5000'
+const ENDPOINT = 'https://suduoku-server.herokuapp.com/'
+// for testing 
+// const ENDPOINT = 'localhost:5000'
 const socket = io(ENDPOINT)
 
 export { socket };
